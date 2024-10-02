2bf23c4a6dfb01a9c081cbbc03bbf29ce2089a71
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-10-02 23:46:58 +0400

    Commit 2

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,7 +1,52 @@
 import React from 'react'
 
+const months = { 'January': 'Jan', 'February': 'Feb', 'March': 
+  'Mar', 'April': 'Apr', 'May': 'May', 'June': 'Jun', 'July': 
+  'Jul', 'August': 'Aug', 'September': 'Sept', 'October': 'Oct',
+  'November': 'Nov', 'December': 'Dec' };
+const now = new Date();
+const dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
+  'Thursday', 'Friday', 'Saturday'];
+
 export default function Calendar() {
+
+  const year = 2024;
+  const month = "July";
+  const monthNow = new Date(year, Object.keys(months).indexOf(month), 1)
+  const firstDayOfMonth = monthNow.getDay();
+  const daysInMonth  = new Date(year, Object.keys(month).indexOf(month) + 1, 0)
+
+  const daysToDisplay = firstDayOfMonth + daysInMonth;
+  const numRows = (Math.floor(daysToDisplay / 7 )) + (daysToDisplay % 7 ? 1 : 0)
+
   return (
-    <div>Calendar</div>
+    <div className='flex flex-col overflow-hidden gap-1'>
+      {[...Array(numRows).keys()].map((row, rowIndex) => {
+        return(
+          <div key={rowIndex}> 
+            {dayList.map((dayOfWeek, dayOfWeekIndex) => {
+              let dayIndex = (rowIndex * 7) + 
+              dayOfWeekIndex - (firstDayOfMonth - 1)
+
+              let dayDisplay = dayIndex > daysInMonth ? 
+              false : (row === 0 && dayOfWeekIndex < firstDayOfMonth) ? false : true
+
+              let isToday = dayIndex === now.getDate()
+
+              if(!dayDisplay) {
+                return (
+                  <div className='bg-white' key={dayOfWeekIndex} />
+                )
+              }
+
+              return(
+                <div key={dayOfWeekIndex}> </div>
+              )
+            })}
+          </div>
+        )
+      })}
+
+    </div>
   )
 }
--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,5 +1,6 @@
 import { Fugaz_One } from 'next/font/google';
 import React from 'react'
+import Calendar from './Calendar';
 
 const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
 
@@ -19,28 +20,30 @@ export default function Dashboard() {
   }
 
   return (
-    <div className='flex flex-col flex-1 gap-4 sm:gap-8 md:gap-12 '>
-      <div className='grid grid-cols-1 sm:grid-cols-3 bg-indigo-50 text-indigo-500 rounded-lg'>
+    <div className='flex flex-col flex-1 gap-8 sm:gap-12 md:gap-16 '>
+      <div className='grid grid-cols-3 bg-indigo-50 text-indigo-500 p-4 gap-4 rounded-lg'>
         {Object.keys(statuses).map((status, statusIndex) => {
+
           return(
-            <div key={statusIndex} className='p-4 flex flex-col gap-1 sm:gap-2 '>
-              <p className='font-medium uppercase text-xs sm:text-sm '> {status.replaceAll("_", " ")} </p>
-              <p className={'text-base sm:text-lg ' + fugaz.className}> {statuses[status]} </p>
+            <div key={statusIndex} className=' flex flex-col gap-1 sm:gap-2 '>
+              <p className='font-medium uppercase text-xs sm:text-sm truncate '> {status.replaceAll("_", " ")} </p>
+              <p className={'text-base sm:text-lg truncate ' + fugaz.className}> {statuses[status]} </p>
             </div>
           )
         } )}
       </div>
       <h4 className={'text-5xl sm:text-6xl md:text-7xl text-center ' + fugaz.className}> How do you <span className='textGradient'>feel</span> today? </h4>
-      <div className='grid grid-cols-2 md:grid-col-5 gap-4'>
+      <div className='flex items-strech flex-wrap gap-4 '>
         {Object.keys(moods).map((mood, moodIndex) => {
           return (
-            <button className={''} key={moodIndex}> 
-              <p> {mood}</p>
-              <p> {moods[mood]}</p>
+            <button className={'p-4 px-8 rounded-2xl purpleShadow duration-200 bg-indigo-50 hover:bg-indigo-100 text-center flex flex-col items-center gap-2 flex-1 '} key={moodIndex}> 
+              <p className='text-4xl sm:text-5xl md:text-6xl'>{moods[mood]}</p>
+              <p className={'text-indigo-500 text-xs sm:text-sm md:text-base ' + fugaz.className}>{mood}</p>
             </button>
           )
         })}
       </div>
+      <Calendar />
     </div>
   )
 }
