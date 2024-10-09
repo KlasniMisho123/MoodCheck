fddbecf9857f9eacf16e86a62ba293095bfe1f68
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-10-09 12:19:58 +0400

    Commit 9

--- components/Loading.js
+++ components/Loading.js
@@ -1,12 +1,10 @@
-import React from 'react';
+import React from 'react'
 
-function Loading() {
+export default function Loading() {
   return (
     <div className='flex flex-col flex-1 justify-center items-center '>
       LOADING...
       <i className="fa-solid text-slate-800 fa-spinner animate-spin text-4xl sm:text-5xl"></i>
     </div>
-  );
+  )
 }
-
-export default Loading;
