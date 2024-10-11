98366bdf23f9ae0f7c9526745a9629b0d02c7b88
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-10-11 22:48:22 +0400

    Footer Commit

--- components/Button.js
+++ components/Button.js
@@ -4,11 +4,11 @@ import { Fugaz_One, Open_Sans } from 'next/font/google';
 const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
 
 export default function Button(props) {
-  const { text, dark, full, clickHandler } = props
+  const { text, dark, full, clickHandler, icon } = props
   
   return (
-    <button onClick={clickHandler} className={' rounded-full overflow-hidden border-2 duration-200 hover:opacity-60 border border-solid border-indigo-600 ' + (dark ? 'text-white bg-indigo-600 ': 'text-indigo-600 ') + (full ? ' grid place-items-center w-full ' : ' ')}>
-        <p className={' px-6 sm:px-10 whitespace-nowrap py-2 sm:py-3 ' + fugaz.className}> {text} </p>
+    <button onClick={clickHandler} className={' rounded-full overflow-hidden border-2 duration-200 hover:opacity-60 border border-solid border-indigo-600 ' + (dark ? 'text-white bg-indigo-600 ': 'text-indigo-600 ') + (full ? ' grid place-items-center w-full ' : ' ')}>  
+        <p className={'px-6 sm:px-10 whitespace-nowrap py-2 sm:py-3 ' + fugaz.className}>{icon} {text}</p>
     </button>
   )
 }
--- components/Footer.js
+++ components/Footer.js
@@ -1,21 +1,46 @@
 import React from 'react'
 import Button from './Button'
 import { Fugaz_One } from 'next/font/google';
+import Link from 'next/link';
 
 
 const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
 
 export default function Footer() {
   return (
-    <footer className="p-4 sm:p-8 grid place-items-center border w-full">
+    <footer className="py-4 sm:py-8 grid place-items-center border w-full ">
         <div className='bg-white border  border-black shadow rounded-[32px] p-6 flex flex-col gap-4 gap-6:md gap-8:lg '>
             <div>
                 <h1 className={"flex gap-1 text-lg sm:text-xl md:text-1xl lg:text-2xl "+ fugaz.className}> <span className={'text-indigo-400 '}> Ready? </span> Start your own business</h1>
                 <p className='text-gray-400 text-xs sm:text-xs md:text-sm lg:text-base '>We are here to start your new project and finish it soon </p>     
             </div>
             <div className='flex justify-center'>
-                <Button text="Contact Us" dark />
+                <Button text={"Contact Us"} icon={<i className="fa-regular fa-clipboard"></i>} dark />
             </div>
+        </div>
+        <div className='bg-green-100 w-full py-4 flex p-4 flex justify-evenly items-center '>
+          <div className='border border-black p-4 '>
+          <Link href={'/'}>
+            <h1 className={"text-base sm:text-lg textGradient " + fugaz.className}> MoodCheck </h1>
+          </Link>
+          <p className='text-sm '> An intuitive web platform that allows you to effortlessly monitor and reflect on your moods and daily experience </p>
+          <div className='flex '>
+            <Link href={'/'}> 
+              <div> <i className="fa-brands fa-linkedin-in "></i> </div>
+            </Link>
+            <Link href={'/'}> 
+              
+            </Link>
+            <Link href={'/'}> 
+              
+            </Link>
+            </div>
+          </div>
+
+          <div>
+            <p>ss</p>
+          </div>
+
         </div>
     </footer>
   )
