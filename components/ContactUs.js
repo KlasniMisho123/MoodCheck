5c32916e68fe88ea5b22097b9dfe8b92b5a7915c
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-10-14 17:53:59 +0400

    Get in touch Commit

--- components/ContactUs.js
+++ components/ContactUs.js
@@ -11,13 +11,14 @@ const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
 const opensans  = Open_Sans ({ subsets: ["latin"], weight: ["400"] });
 
 export default function ContactUs() {
+    const [contactName, setContactName] = useState("")
     const [contactEmail, setContactEmail ] = useState("")
     const [contactSubject, setContactSubject ] = useState("")
     const [contactText, setContactText ] = useState("")
-
     const { currentUser, loading } = useAuth()
 
     function handleContactSubmit() {
+        console.log(`Name: ${contactName}`)
         console.log("Email: ", contactEmail)
         console.log("Subject: ", contactSubject)
         console.log("Text: ", contactText)
@@ -35,13 +36,51 @@ export default function ContactUs() {
 
     return (
     <div className=''>
-        
-        <h1 className={'my-[40px] sm:text-xl md:text-2xl lg:text-3xl text-center ' + fugaz.className} > We <span className='textGradient '> Value </span> Your Feedback <i className="fa-regular fa-comment-dots ml-2 textGradient"></i></h1>
+        <section className='w-full bg-indigo-200 p-4 min-h-[500px]'>
+            {/* GET IN TOUCH SECTION */}
+            <h1 className={'text-center p-6 sm:text-lg md:text-xl lg:text-2xl ' + fugaz.className }> Get in touch </h1>
+            <div className='flex flex-row justify-evenly mt-4 '>
+                <div className='flex flex-col gap-4 text-center '>
+                    <div> <i className="fa-solid fa-location-dot text-red-500 text-2xl "></i> </div>
+                    <h1>ADDRESS</h1>
+                    <div className='flex flex-col gap-6 '>
+                        <span>SS</span>
+                        <span>S2</span>
+                    </div>
+                </div>
+
+                <div className='border-x border-gray-500 '>s2</div>
+                <div className='border-l border-gray-500 '>s3</div>
+            </div>
+        </section>
+
+
+        <hr className='my-[40px] border-indigo-500 '/>
+
+        <div className='max-w-[600px] mx-auto w-full'>
+            <Link href={'/dashboard'}>
+                <Button dark full text='Go to dashboard' />
+            </Link>
+        </div>
+
+        <h1 className={'my-[40px] sm:text-xl md:text-2xl lg:text-3xl text-center ' + fugaz.className} >
+             We <span className='textGradient '> Value </span> Your Feedback 
+        <i className="fa-regular fa-comment-dots ml-2 textGradient"></i></h1>
 
         <div className='w-full flex justify-center p-4 '>
             
-            <div className='bg-indigo-100 flex flex-col items-center gap-4 p-6 h-[500px] w-[400px] rounded-lg border border-black'>
-                <input className={'border border-black mt-4 rounded-sm p-[2px] ' + opensans.className }
+            <div className='bg-indigo-100 flex flex-col items-center gap-4 p-6 h-[500px] w-[400px] rounded-lg border border-black '>
+
+            <input className={'border border-black mt-4 rounded-sm p-[2px] ' + opensans.className }
+                placeholder='Name'
+                type='text' 
+                value={contactName}
+                onChange={(e) => {
+                    setContactName(e.target.value)
+                }}
+                />
+
+                <input className={'border border-black rounded-sm p-[2px] ' + opensans.className }
                 placeholder='Email'
                 type='text' 
                 value={contactEmail}
@@ -81,16 +120,6 @@ export default function ContactUs() {
                 />    
             </div>
         </div>
-
-        <hr className='my-[40px] border-indigo-500 '/>
-
-        <div className='max-w-[600px] mx-auto w-full'>
-            <Link href={'/dashboard'}>
-                <Button dark full text='Go to dashboard' />
-            </Link>
-        </div>
-        <hr className='my-[40px] border-indigo-500 '/>
-
     </div>
     
     )
