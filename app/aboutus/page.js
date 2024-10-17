37fc8bfe722ba54c5a0e8e9b2d2d4a00b97d1ab4
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-10-17 20:49:05 +0400

    About Us Page Commit

---
+++ app/aboutus/page.js
@@ -0,0 +1,18 @@
+import Aboutus from '@/components/Aboutus';
+import Main from "@/components/Main"
+import React from 'react'
+
+
+export const metadata = {
+    title: "MoodCheck ⋅ About Us"
+};
+
+export default function AboutUsPage() {
+
+  return (
+    <Main> 
+        <Aboutus />
+    </Main>
+
+  )
+}
