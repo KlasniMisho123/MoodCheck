66523cccba0a2ff80963d28d20d2756eff7658a2
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-10-17 20:34:31 +0400

    Register/Login Response Commit

--- components/ContactUs.js
+++ components/ContactUs.js
@@ -71,11 +71,11 @@ export default function ContactUs() {
 
     return (
     <div className=''>
-        <section className='w-full bg-indigo-400 p-4 min-h-[500px] rounded-lg '>
+        <section className='w-full bg-indigo-300 p-4 min-h-[500px] min-w-[400px] rounded-lg '>
             
-            <h1 className={'text-center p-6 sm:text-lg md:text-xl lg:text-2xl ' + fugaz.className }> Help us to improve  </h1>
+            <h1 className={'text-center p-6 text-gray-100 sm:text-lg md:text-xl lg:text-2xl ' + fugaz.className }> Help us to improve 😊 </h1>
             
-            <div className='flex flex-row justify-evenly mt-4 '>
+            <div className='flex flex-row justify-evenly mt-4 gap-4'>
                 <GetInTouchElement 
                     entryNumber={1}
                     title="ADDRESS"
