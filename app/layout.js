2f4d6f3c7fac4d56b5a286a8f8e8743affb6fcd3
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-02-04 22:34:19 +0400

    header commit

--- app/layout.js
+++ app/layout.js
@@ -2,16 +2,15 @@ import localFont from "next/font/local";
 import { Fugaz_One, Open_Sans, Poppins } from 'next/font/google';
 import { AuthProvider } from "@/context/AuthContext";
 import "./globals.css";
-import Link from "next/link";
 import Head from "./head";
-import Logout from "@/components/Logout";
+
 import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 
 
 const opensans  = Open_Sans ({ subsets: ["latin"], weight: ["400"] });
 const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
 const poppins = Poppins({ subsets: ["latin"], weight: ["700"] });
-
 
 export const metadata = {
   title: "MoodCheck",
@@ -21,25 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 
   const header = (
-    <header className="p-4 sm:p-8 flex items-center justify-between gap-4">
-      <Link href={'/'}>
-        <h1 className={"text-base sm:text-lg textGradient " + fugaz.className}> MoodCheck </h1>
-      </Link>
-      <div className="flex flex-row items-center gap-4 ">
-        <div className="flex flex-row min-w-[300px] justify-evenly gap-4 ">
-          <Link href={'/dashboard'}>
-            <p className={"text-indigo-500 text-base overflow-hidden whitespace-nowrap navSection " + fugaz.className}> Dashboard </p>
-          </Link>
-          <Link href={'/contactus'}>
-            <p className={"text-indigo-500 text-base overflow-hidden whitespace-nowrap navSection " + fugaz.className}> Contact Us</p>
-          </Link>
-          <Link href={'/aboutus'}>
-            <p className={"text-indigo-500 text-base overflow-hidden whitespace-nowrap navSection " + fugaz.className }> About Us</p>
-          </Link>
-        </div>
-          <Logout /> 
-      </div>
-    </header>
+    <Header />
   )
 
   const footer = (
---
+++ components/Header.js
@@ -0,0 +1,40 @@
+'use client'
+import React from 'react'
+import { useState } from "react";
+import Logout from "@/components/Logout";
+import Link from "next/link";
+import { Fugaz_One, Open_Sans, Poppins } from 'next/font/google';
+
+const opensans  = Open_Sans ({ subsets: ["latin"], weight: ["400"] });
+const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
+const poppins = Poppins({ subsets: ["latin"], weight: ["700"] });
+
+
+export default function Header() {
+    const [isDay, setIsDay] = useState(true)
+  return (
+    <header className="p-4 sm:p-8 flex items-center justify-between gap-4">
+    <Link href={'/'}>
+      <h1 className={"text-base sm:text-lg textGradient " + fugaz.className}> MoodCheck </h1>
+    </Link>
+    <div className="flex flex-row items-center gap-4 ">
+      <div className="flex flex-row min-w-[300px] justify-evenly gap-4 ">
+        <Link href={'/dashboard'}>
+          <p className={"text-indigo-500 text-base overflow-hidden whitespace-nowrap navSection " + fugaz.className}> Dashboard </p>
+        </Link>
+        <Link href={'/contactus'}>
+          <p className={"text-indigo-500 text-base overflow-hidden whitespace-nowrap navSection " + fugaz.className}> Contact Us</p>
+        </Link>
+        <Link href={'/aboutus'}>
+          <p className={"text-indigo-500 text-base overflow-hidden whitespace-nowrap navSection " + fugaz.className }> About Us</p>
+        </Link>
+      </div>
+        <Logout /> 
+      <div>
+        <i className="fa-solid fa-sun theme-icon day"></i>
+        <i className="fa-solid fa-moon theme-icon night"></i>
+      </div>
+    </div>
+  </header>
+  )
+}
