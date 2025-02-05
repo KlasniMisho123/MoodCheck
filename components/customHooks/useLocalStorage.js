20f28bd317baa48c4e8777909fbcff306d9800ef
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-02-05 17:14:16 +0400

    custom hook changes

--- components/customHooks/useLocalStorage.js
+++ components/customHooks/useLocalStorage.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 
 function getSavedValue(key, initialValue) {
+    if (typeof window === "undefined") return initialValue; 
+
     const savedValue = localStorage.getItem(key);
-    
     if (savedValue !== null) {
         if (savedValue === "true") return true;
         if (savedValue === "false") return false;
@@ -17,7 +18,9 @@ export default function useLocalStorage(key, initialValue) {
     const [value, setValue] = useState(() => getSavedValue(key, initialValue));
 
     useEffect(() => {
-        localStorage.setItem(key, value);
+        if (typeof window !== "undefined") {
+            localStorage.setItem(key, value);
+        }
     }, [key, value]);
 
     return [value, setValue];
